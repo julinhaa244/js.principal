55abbbad6f7786cd004928eccb33824820b13c18
julinhaa244 <163298923+julinhaa244@users.noreply.github.com>
2024-09-25 09:39:58 -0300

    imc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,15 @@
 var titulo= document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
+var pacientes= document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++){
+var paciente = pacientes [i];
+var tdPeso = paciente.querySelector("info-peso");
+var peso = tdpeso.textContent;
+var tdAltura = paciente.querySelector("info-altura");
+var altura = tdAltura.textContent;
+var tdImc = paciente.querySelector(info.imc);
+
+}
+
+]
